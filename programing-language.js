98dd96bae96c01ed470796a7ae41953e36a50757
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-09-07 16:39:03 +0300

    Update programing-language.js

--- programing-language.js
+++ programing-language.js
@@ -1,23 +1,23 @@
 setInterval(SuperDiamondProgramingCore.UpdateBody, 0)
-var SuperDiamondPrograming = [
 function interval(func, time, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10) {
  return setInterval(func, time, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
 }
-function elements.createElement(tag) {
+var elements = [
+function createElement(tag) {
  return document.createElement(tag);
 }
 
-function elements.editElement(elem, type, value) {
+function editElement(elem, type, value) {
  eval("elem." + type + " = " + value)
 }
 
-function elements.getElementData(elem, type) {
+function getElementData(elem, type) {
  return eval("elem." + type);
 }
 var body = document.body
 ]
 ]
 var SuperDiamondProgramingCore = [ function UpdateBody() {
- SuperDiamondPrograming.elements.body = document.body
+ elements.body = document.body
 }
 ]
