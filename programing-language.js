33b245d91140a114689ec8bcdb19a1f2e26fc814
Alex <142623358+Alexidians@users.noreply.github.com>
2023-09-26 17:07:13 +0300

    Update programing-language.js

--- programing-language.js
+++ programing-language.js
@@ -9,58 +9,54 @@ const dates = {
  }
 };
 const intervals = [
-function interval(func, time, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10) {
+interval: function interval(func, time, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10) {
  return setInterval(func, time, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
 }
 
-function timeout(func, time, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10) {
+timeout: function (func, time, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10) {
  return setTimeout(func, time, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
 }
 
-function clearInterval(interval) {
+clear: function (interval) {
  return clearInterval(interval);
 }
 ]
 var elements = [
-function createElement(tag) {
+createElement: function (tag) {
  return document.createElement(tag);
 }
 
-function editElement(elem, type, value) {
+editElement: function (elem, type, value) {
  eval("elem." + type + " = " + value)
 }
 
-function getElementData(elem, type) {
+getElementData: function (elem, type) {
  return eval("elem." + type);
 }
-
-var body = document.body
 ]
 const consol = [
-function error(message) {
+error: function (message) {
  console.error(message)
 }
 
-function info(message) {
+info: function (message) {
  console.info(message)
 }
 
-function warning(message) {
+warning: function (message) {
  console.warn(message)
 }
 
-function debug(message) {
+debug: function (message) {
  console.debug(message)
 }
 
-function clear() {
+clear: function () {
  console.clear()
 }
-
-var memory = console.memory
 ]
 const popups = [
-  function create(url, windowname) { 
+  create: function (url, windowname) { 
     if (! window.focus)return true;
     var href;
     if (typeof(mylink) == 'string') href=mylink;
@@ -69,30 +65,57 @@ const popups = [
     return false; 
   }
 ]
-var cookies = document.cookie
+var cookies = {
+ setCookie: function (name,value,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+getCookie: function (name) {
+  let namet = name + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(namet) == 0) {
+      return c.substring(namet.length, c.length);
+    }
+  }
+  return "";
+}
+ deleteCookie: function (name) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = name + "=;expires=Thu, 01 Jan 1970"
+ }
+}
 const sesStorage = [
- function setItem(name, value) {
+ setItem: function (name, value) {
   sessionStorage.setItem(name, value)
  }
- function getItem(name) {
+ getItem: function (name) {
   return sessionStorage.getItem(name);
  }
- function removeItem(name) {
+ removeItem: function (name) {
   sessionStorage.removeItem(name)
  }
- var itemAmount = sessionStorage.length
 ]
 const storage = [
- function setItem(name, value) {
+ setItem: function (name, value) {
   localStorage.setItem(name, value)
  }
- function getItem(name) {
+ getItem: function (name) {
   return localStorage.getItem(name);
  }
- function removeItem(name) {
+ removeItem: function (name) {
   localStorage.removeItem(name)
  }
- var itemAmount = localStorage.length
 ]
 setInterval(eval, 0, "elements.body = document.body")
 setInterval(eval, 0, "consol.memory = console.memory")
