7f90653035fb357e459f7eeac01a8473baee9186
Alex <142623358+Alexidians@users.noreply.github.com>
2023-12-02 14:39:51 +0200

    Create offline-access.js

---
+++ SuperDiamondPrograming/offline-access.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'SuperDiamondPrograming-offline';
+const CACHE_DATA = [
+  '/config.json',
+  '/user.json',
+  '/service-worker-executor.js',
+  '/offline-access.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_DATA);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
